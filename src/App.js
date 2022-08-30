e768a0910a4decdaf8bc9e02eaf35d3c0e8597e4
Prizmor <yy3980075@gmail.com>
2022-08-30 15:29:20 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import cx from './App.models.scss';
+import Route from './router';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Route/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/router.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const Route = () => {
+    return (
+        <Router>
+
+        </Router>
+    )
+}
+
+export default Router;
